27dbc344dc40fcf4471bd0965f9c9a957594c552
Kapil bavisiya <kapilbavisiya555@gmail.com>
2020-12-03 18:15:29 +0530

    gRPC unary

---
+++ greet/greet_client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Kapil22c/go_grpc/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+
+	fmt.Printf("Created client: %f", c)
+}
